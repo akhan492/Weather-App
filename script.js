600050d18efcfdd9a0b92f110a016cd049a1fab0
arsalan khan <akhan492@gmail.com>
2022-02-01 17:58:34 +0530

    when enter hit button trigger

--- script.js
+++ script.js
@@ -66,3 +66,9 @@ setInterval(timeShow, 1000);
       "url('https://images.pexels.com/photos/3052361/pexels-photo-3052361.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260')";
   }
 })();
+city.addEventListener("keyup", (e)=>{
+  if(e.keyCode===13){
+    e.preventDefault();
+    button.click();
+  }
+})
